6cb681d115f04bb3509c4a8eab49629cb9b737a1
dpacker123 <89939888+dpacker123@users.noreply.github.com>
2021-10-22 09:05:23 -0400

    BusinessCustomer complete

--- src/comp/comp152/BusinessCustomer.java
+++ src/comp/comp152/BusinessCustomer.java
@@ -1,34 +1,49 @@
 package comp.comp152;
 
-import javax.naming.Name;
 import java.util.ArrayList;
 
-public class BusinessCustomer extends Customer{
+public class BusinessCustomer extends Customer {
 
     private double purchaseOrderBalance;
-
-
-    //@Override
-  //  public double PayForOrder(ArrayList<ItemForSale> itemsInOrder) {
-       // System.out.println("" + getName() + purchaseOrderBalance);
-
- //   }
 
     public BusinessCustomer(String Name, int ID) {
         super(Name, ID);
     }
 
-    public double PayOutstandingBalance(){
-        if(payOutstandingBalance() > 1000){
-            var newOutStandingBalance = payOutstandingBalance() * .05;
-            System.out.println("The new oustanding balance is" + newOutStandingBalance);
-            return 0.0;
-        }
-        else{
-            System.out.println("The outstanding balance was below 1000");
-            return 0.0;
-        }
+    public BusinessCustomer(String custName) {
+        super(custName);
     }
 
 
+    public double PayForOrder(ArrayList<merchandiseItem> itemsInOrder) {
+        double sum = 0;
+        for (merchandiseItem item : itemsInOrder) {
+            ItemType type = item.getType();
+            if (ItemType.GeneralMerchandise == type) {
+                sum += item.getPrice() + item.getPrice()*0.0625;
+            }
+            else if (ItemType.Clothing == type && item.getPrice() > 175) {
+                sum += item.getPrice() + (item.getPrice() - 175)*0.0625;
+            }
+        }
+        this.purchaseOrderBalance = sum;
+        System.out.printf("This order will be for "+getName()+
+                " and cumulative price of the order (including taxes) is $%,.2f\n",sum);
+        return 0;
+    }
+
+
+    public double payOutstandingBalance() {
+        double balance = this.purchaseOrderBalance;
+        if (balance > 1000) {
+            balance = balance*0.95;
+        }
+        this.purchaseOrderBalance = 0;
+        return balance;
+    }
+
+
+    public void arrangeDelivery() {
+        System.out.println("["+getName()+"] all deliveries must be from 9-5 Mon-Fri");
+    }
 }
